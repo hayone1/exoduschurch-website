5b573e32fea42436ad00a8cc104eb0329961d680
hayone1 <boluwatife.morawo@stu.cu.edu.ng>
2025-05-07 12:44:07 +0100

    working

--- app/components/layout/Header.vue
+++ app/components/layout/Header.vue
@@ -1,81 +1,90 @@
 <script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui';
+
 // import type { NavigationMenuItem } from '@nuxt/ui'
 const viewport = useViewport();
+const navLabels = [
+  "Community", "Sermons", "Events",
+  "Give", "Reach out"
+];
 
-watch(viewport.breakpoint, (newBreakpoint, oldBreakpoint) => {
+function navProps(label: string | undefined) {
+  var navClass: string;
+  var navLabel: string;
+  var borderSize: string;
   if (viewport.isLessThan('mobileWide')) {
-        items.forEach(item => {
-            item.class = "text-lg";
-            item.label = "";
-        })
-    }
-    else if (viewport.isLessThan('tablet')) {
-        items.forEach(item => {
-            item.class = "text-xs";
-            item.label = item.value;
-        })
+    navClass = "text-lg";
+    navLabel = "";
+    borderSize = "0";
+  }
+  else if (viewport.isLessOrEquals('tablet')) {
+    navClass = "text-xs";
+    navLabel = label ?? "";
+    borderSize = "1";
   }
   else {
-    items.forEach(item => {
-        item.label = item.value;
-        item.class = "text-base";
-    })
+    navLabel = label ?? "";
+    navClass = "text-base";
+    borderSize = "1";
   }
-})
+  return { navClass, navLabel, borderSize };
+};
 
+// watch(viewport.breakpoint, (newBreakpoint, oldBreakpoint) => {
 
-const items = reactive<NavigationMenuItem[]>([
+// })
+
+const borderSize = ref("1");
+const defaultNavItems = reactive<NavigationMenuItem[]>([
   {
-    label: 'Community',
-    value: 'Community',
+    label: navProps(navLabels[0]).navLabel,
     icon: 'i-fluent-people-community-20-regular',
     to: '/getting-started',
-    class: 'text-base',
-    children: []
-  },
-  {
-    label: 'Sermons',
-    value: 'Sermons',
-    icon: 'i-fluent-headphones-sound-wave-20-filled',
-    to: '/composables',
-    class: 'text-base',
+    class: navProps(navLabels[0]).navClass,
     active: true,
     children: []
   },
   {
-    label: 'Events',
-    value: 'Events',
-    icon: 'i-fluent-calendar-20-regular',
-    to: '/components',
-    class: 'text-base',
-    active: false,
+    label: navProps(navLabels[1]).navLabel,
+    icon: 'i-fluent-headphones-sound-wave-20-filled',
+    to: '/composables',
+    class: navProps(navLabels[1]).navClass,
     children: []
   },
   {
-    label: 'Give',
-    value: 'Give',
+    label: navProps(navLabels[2]).navLabel,
+    icon: 'i-fluent-calendar-20-regular',
+    to: '/components',
+    class: navProps(navLabels[2]).navClass,
+    children: []
+  },
+  {
+    label: navProps(navLabels[3]).navLabel,
     icon: 'i-fluent-handshake-20-regular',
     to: 'https://github.com/nuxt/ui',
-    class: 'text-base',
+    class: navProps(navLabels[3]).navClass,
     target: '_blank'
   },
   {
-    label: 'Reach out',
-    value: 'Reach out',
+    label: navProps(navLabels[4]).navLabel,
+    to: '/components',
     icon: 'i-fluent-mail-multiple-20-regular',
-    class: 'text-base',
+    class: navProps(navLabels[4]).navClass,
   }
-])
+]);
+
+// const navItems = reactive<NavigationMenuItem[]>(allNavItems);
+
+
 </script>
 
 <template>
-    <UContainer class="pt-2">
-        <div class="rounded-full border-solid border-secondary border-1 max-w-xl mx-auto
-                    transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-neutral-800">
-            <UNavigationMenu :items="items" variant="link"
-                highlight class="flex justify-center-safe"
-                />
+  <UContainer class="pt-4">
+    <UContainer :class="`rounded-full border-solid border-secondary border-${borderSize} mx-auto
+                    transition delay-150 duration-300 ease-in-out hover:-translate-y-1
+                    hover:scale-105 hover:bg-neutral-800`">
+      <UNavigationMenu :items="defaultNavItems" variant="link" highlight class="flex justify-center-safe" />
 
-        </div>
     </UContainer>
+  </UContainer>
 </template>
--- exodus-readme.md
+++ exodus-readme.md
@@ -13,23 +13,28 @@
 - https://soubiran.dev/posts/the-perfect-guide-to-setting-up-a-new-nuxt-project
 
 ### Inspiration
-- https://www.pinterest.com/pin/69031806784478238/
-- https://www.pinterest.com/pin/17310779813714518/
-- https://www.pinterest.com/pin/838232549430603509/
-- https://www.pinterest.com/pin/43347215160116229/
 - Tags - https://www.pinterest.com/pin/11470174046180232/
 - tags Background - https://www.pinterest.com/pin/6544361954011327/
-- https://www.pinterest.com/pin/13862711349179664/
-- https://www.pinterest.com/pin/9007268000004824/
-- https://www.pinterest.com/pin/5488830792098394/
-- https://www.pinterest.com/pin/6685099441218958/
-- https://centralchristianocala.org/new-here/
-- https://webflow.com/templates/html/godsgrace-church-website-template?utm_medium=affiliate&utm_source=stevebenjamins6665&ps_partner_key=c3RldmViZW5qYW1pbnM2NjY1&ps_xid=Jbn9rOjk5gB1XX&gsxid=Jbn9rOjk5gB1XX&gspk=c3RldmViZW5qYW1pbnM2NjY1
-- https://sm-cove-demo.squarespace.com/?password=smdemo
 - https://anchortacoma.org/
+- https://sm-cove-demo.squarespace.com/?password=smdemo
 - https://www.elevationchurch.org/
-- https://dribbble.com/shots/19031634-Lifepoint-Church-Website
+- https://centralchristianocala.org/new-here/
+- https://centralchristianocala.org/new-here/
+- https://www.pinterest.com/pin/5629568279173198/
+- https://www.pinterest.com/pin/1759287348204194/
+- https://www.pinterest.com/pin/752734525258299590/
+- https://www.pinterest.com/pin/1105070827318456944/
+- https://www.pinterest.com/pin/108649409754171952/
+- https://www.pinterest.com/pin/21321798231009285/
+- https://www.pinterest.com/pin/20125529579545135/
+- https://www.pinterest.com/pin/4925880837041312/
+- https://www.pinterest.com/pin/6685099441218958/
+- 
 
+## More Inspiration
+- https://canvas.hrcd.fr/en
+- file:///C:/Users/Admin/Downloads/envato/bentofolio-personal-portfolio-html-template/main-file/index.html
+- https://prismic.io/blog/css-image-effects#44-image-hover-effect
 ## Backgrounds
 - https://www.freepik.com/free-photo/black-abstract-texture-background_1273439.htm#fromView=search&page=1&position=8&uuid=de412740-834b-41be-8dc1-5ee568d4c6a7&query=Grain+Background
 
